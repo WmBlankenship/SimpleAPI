3d87c09696d2ffd9cbc1dc79931156ac2cf24e5c
William Blankenship <blankenship.william@gmail.com>
2017-09-16 08:43:42 -0400

    Infrastructure project added Simple Result Service added

---
+++ Infrastructure/Services/SimpleResultService.cs
@@ -0,0 +1,20 @@
+﻿using SimpleAPI.Core.Interfaces;
+using SimpleAPI.Core.Models;
+using System;
+
+namespace Infrastructure.Services
+{
+    public class SimpleResultService : IResultService
+    {
+        public SimpleResult GetResult()
+        {
+            var result = new SimpleResult
+            {
+                Message = "This result was accessed at ",
+                Timestamp = DateTimeOffset.UtcNow
+            };
+
+            return result;
+        }
+    }
+}
