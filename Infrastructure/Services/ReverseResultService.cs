bfbe2d9fd7fe31d51ea3eca25370bc9b774a3bb2
William Blankenship <blankenship.william@gmail.com>
2017-09-16 08:59:20 -0400

    ReverseResultService added

---
+++ Infrastructure/Services/ReverseResultService.cs
@@ -0,0 +1,24 @@
+﻿using SimpleAPI.Core.Interfaces;
+using SimpleAPI.Core.Models;
+using System;
+
+namespace Infrastructure.Services
+{
+    public class ReverseResultService : IResultService
+    {
+        public SimpleResult GetResult(string input)
+        {
+            var chars = input.ToCharArray();
+            Array.Reverse(chars);
+            var reverse = chars.ToString();
+
+            var result = new SimpleResult
+            {
+                Input = input,
+                Output = reverse
+            };
+
+            return result;
+        }
+    }
+}
