81b22d80a91409687f3aba8069348f48618a5b20
William Blankenship <blankenship.william@gmail.com>
2017-09-16 08:08:23 -0400

    Simple Get implemented Naming Routing

---
+++ SimpleAPI/Controllers/SimpleController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace SimpleAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class SimpleController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            var timestamp = DateTimeOffset.UtcNow;
+
+            return $"This API was accessed at {timestamp}";
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
